085be9985c1c3375e57d45a24c721a11b1880981
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 17:28:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
     @amount = amount
   end
 
-  def valid?(sender, receiver)
+  def valid?
     sender_status = BankAccount.new(@sender).valid?
     receiver_status = BankAccount.new(@receiver).valid?
     if sender_status == true && receiver_status == true
