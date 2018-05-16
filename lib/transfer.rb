65ff51307feeeb81deac999083a9bd42fd58ed96
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 17:52:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.deposit(@amount)
       @status = "complete"
+    end
     elsif valid? == false
       return "Transaction rejected. Please check your account balance."
       @status = "rejected"
