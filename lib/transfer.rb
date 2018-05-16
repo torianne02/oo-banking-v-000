fcc7c10877635a0e36828fc7390ddcfc89f76adc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 17:57:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,8 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    @sender.balance = @sender.deposit(@amount)
+    @receiver.balance = @receiver.balance - @amount
 end
