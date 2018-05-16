1bfa1ed565d6cdf31085461f52ef27a487f92b10
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 17:19:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end
 end
