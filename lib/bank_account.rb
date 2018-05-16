6a02103a34966e56fa021ef6225b58de3a4bed77
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-16 17:10:03 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,14 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
+  
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit(money)
+    @balance += money
   end
 end
